e1b3cb4d86ba5ea06bc66f69d7571d933f4622cb
Brian Faust <faustbrian@users.noreply.github.com>
2017-06-06 11:01:57 +0300

    Apply fixes from StyleCI (#8)
    [ci skip] [skip ci]

--- src/GeoIp2ServiceProvider.php
+++ src/GeoIp2ServiceProvider.php
@@ -25,7 +25,7 @@ class GeoIp2ServiceProvider extends ServiceProvider
         ], 'config');
 
         $this->publishes([
-            __DIR__.'/../data/GeoLite2-City.mmdb' => storage_path('app/GeoIp2/GeoLite2-City.mmdb'),
+            __DIR__.'/../data/GeoLite2-City.mmdb'    => storage_path('app/GeoIp2/GeoLite2-City.mmdb'),
             __DIR__.'/../data/GeoLite2-Country.mmdb' => storage_path('app/GeoIp2/GeoLite2-Country.mmdb'),
         ], 'data');
     }
